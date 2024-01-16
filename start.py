93685ede998ab69edea006ff67a7b4eadaab596c
zengya55 <68380292+zengya55@users.noreply.github.com>
2024-01-16 15:11:44 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import os
-os.python('python create_db.py')
+
 os.system('python download_hf.py')
+os.python('python create_db.py')
 os.system('streamlit run app.py --server.address=0.0.0.0 --server.port 7860')
